48bc7092656057d8710c5bfd1c39423e4e36e60f
TakumaShiga <tak07sweetvo@gmail.com>
2021-05-20 17:55:23 +0900

    バリデーション正規表現

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,12 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
-  validates :family_name, presence: true
-  validates :first_name, presence: true
-  validates :family_name_k, presence: true
-  validates :first_name_k, presende: true
+  validates_format_of :password, with: PASSWORD_REGEX
+  with_options presence: true do
+    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :family_name_k, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :first_name_k, format: { with: /\A[ァ-ヶー－]+\z/ }
+  end
   validates :birth_day, presence: true
 end
