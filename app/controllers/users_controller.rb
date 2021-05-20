0ee7f6684c2143502f8539a59f2c1a31a3a10471
TakumaShiga <tak07sweetvo@gmail.com>
2021-05-21 07:12:00 +0900

    ユーザーコントローラー設定

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :nickname, :email, :password,
+      :family_name, :first_name, :family_name_k, :first_name_k, :birth_day)
+  end
+
+end
