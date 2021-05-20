ee3f70be77edeb8273eb7ed6ecb98202b87ec19d
TakumaShiga <tak07sweetvo@gmail.com>
2021-05-20 15:09:20 +0900

    devise_parameter_sanitizerの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_paramerters, if: :devise_controller?
+
+  private
+  def configure_permitted_paramerters
+    devise_parameter_sanitizer.permit(
+      :sign_up, keys: [:nickname, :family_name, :first_name, 
+      :family_name_k, :first_name_k, :birth_day])
+  end
 end
