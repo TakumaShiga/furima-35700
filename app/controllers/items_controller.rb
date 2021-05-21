f19bb42436714cb395aa04e0e3dd5d5267da7fb6
TakumaShiga <tak07sweetvo@gmail.com>
2021-05-22 08:19:43 +0900

    createアクションとストロングパラメーターの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,6 +9,7 @@ before_action :move_to_login, only: [:new, :create]
   end
 
   def create
+    @item = Item.create(item_params)
   end
 
 
@@ -17,5 +18,12 @@ before_action :move_to_login, only: [:new, :create]
      redirect_to new_user_session_path
     end
   end
-
+  
+  private
+  def item_params
+    params.require(:item).permit(
+      :name, :description,:price, :category_id, 
+      :condition_id, :shipping_burden_id, :prefecture_id,
+      :shipping_date_id).merge(user_id: current_user.id)
+    end
 end
