ef846c1ebd377ed2c908db20bfd1b489549300b8
TakumaShiga <tak07sweetvo@gmail.com>
2021-05-21 16:18:56 +0900

    バリデーション with_option

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,21 +4,23 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :email, uniqueness: true
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
 
-  with_options presence: true do
-    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: 'には全角（ひらがな、カタカナ、漢字）で入力してください' } do
-    end
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: 'には全角（ひらがな、カタカナ、漢字）で入力してください'} do
-    end
-    validates :family_name_k, format: { with: /\A[ァ-ヶー－]+\z/, message: 'はカタカナで入力して下さい。' } do
-    end
-    validates :first_name_k, format: { with: /\A[ァ-ヶー－]+\z/, message: 'はカタカナで入力して下さい。' } do
-    end
+  with_options format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: 'には全角（ひらがな、カタカナ、漢字）で入力してください' } do
+    validates :family_name
+    validates :first_name
   end
 
-  validates :birth_day, presence: true
+  with_options format: { with: /\A[ァ-ヶー－]+\z/, message: 'はカタカナで入力して下さい。'} do
+    validates :family_name_k
+    validates :first_name_k
+  end
+
+  with_options presence: true  do 
+    validates :nickname
+    validates :birth_day
+  end  
+
+  validates :email, uniqueness: true
 end
