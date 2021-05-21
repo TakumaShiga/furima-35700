61069f74022d08b1de07594d31db5ebd183df3b3
TakumaShiga <tak07sweetvo@gmail.com>
2021-05-22 07:19:39 +0900

    コントローラー newアクション

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,21 @@
 class ItemsController < ApplicationController
-  
+before_action :move_to_login, only: [:new, :create]
+
   def index
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+  end
+
+
+  def move_to_login
+    unless user_signed_in?
+     redirect_to new_user_session_path
+    end
   end
 
 end
