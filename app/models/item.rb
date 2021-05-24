3b1a7c782b14ccc443afd4905d35f2baebc980ac
TakumaShiga <tak07sweetvo@gmail.com>
2021-05-24 18:27:02 +0900

    エラーメッセージ表示バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,33 +1,28 @@
 class Item < ApplicationRecord
-  belongs_to :user
-  has_one_attached :image
-
-  with_options presence: true do
-    validates :image
-    validates :name
-    validates :description
-  end
-  
-  with_options presence: true, format: { with: /\A[0-9]+\z/ } do
-    validates :price, numericality: { 
-    only_integer: true, greater_than_equal_to: 300,
-    less_than_equal_to: 9_999_999 
-    }
-  end
-
-  with_options numericality: { other_than: 1} do
-    validates :category_id        
-    validates :condition_id      
-    validates :prefecture_id    
-    validates :shipping_burden_id 
-    validates :shipping_date_id 
-  end
-
   extend ActiveHash::Associations::ActiveRecordExtensions
     belongs_to :category
     belongs_to :condition
     belongs_to :prefecture 
     belongs_to :shipping_burden 
     belongs_to :shipping_date 
-    
+  
+  belongs_to :user
+  has_one_attached :image
+
+  validates :price, presence: true, numericality: { with: /\A[0-9]+\z/, message: 'は半角数字で入力してください' } 
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'は300円以上9,999,999円以下を入力してください'}
+
+  with_options presence: true do
+    validates :name
+    validates :image
+    validates :description
+  
+    with_options numericality: { other_than: 1, message: 'を選択してください'} do
+      validates :category_id        
+      validates :condition_id      
+      validates :prefecture_id    
+      validates :shipping_burden_id 
+      validates :shipping_date_id 
+    end
+  end
 end
