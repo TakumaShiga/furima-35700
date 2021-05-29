fc26dd8f9f4edb16bca196e2341578f601ee8108
TakumaShiga <tak07sweetvo@gmail.com>
2021-05-29 21:25:27 +0900

    電話番号はstringのため "" を追加

--- spec/factories/purchase_addresses.rb
+++ spec/factories/purchase_addresses.rb
@@ -5,9 +5,7 @@ FactoryBot.define do
     city { '東京都' }
     house_number { '1-1' }
     building_name { '東京ハイツ' }
-    phone_number {12345678901}
+    phone_number {"12345678901"}
     token {"tok_abcdefghijk00000000000000000"}
-    association :user
-    association :item
   end
 end
